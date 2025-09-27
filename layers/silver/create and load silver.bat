de1d8cfe5e02bce783444497079f3d611289fe8d
Nishant Singh <singhnishant414243@gmail.com>
2025-09-27 19:00:00 +0530

    update file path in .bat files

--- layers/silver/create and load silver.bat
+++ layers/silver/create and load silver.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Create and Load Silver Layer into MySQL...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\layers\bronze\create and load silver.sql"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\layers\silver\create and load silver.sql"
 pause
