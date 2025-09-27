deb45fbb610a2c23a04731c64fe8c19631979a15
Nishant Singh <singhnishant414243@gmail.com>
2025-09-27 11:20:53 +0530

    correct file path in .bat files

--- layers/bronze/create and load bronze.bat
+++ layers/bronze/create and load bronze.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Create and Load Bronze Layer into MySQL...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\create and load bronze.sql"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\layers\bronze\create and load bronze.sql"
 pause
