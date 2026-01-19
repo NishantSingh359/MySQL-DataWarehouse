af6a1f83ed641c0fa97c078bf418995b104f6eb6
Nishant Singh <singhnishant414243@gmail.com>
2026-01-19 19:52:34 +0530

    change folder

---
+++ scripts/run_bronze.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Create and Load Bronze Layer into MySQL...
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\bronze.sql"
+pause
+
