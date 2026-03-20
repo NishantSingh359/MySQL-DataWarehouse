6c49fd4c05efb29a904116db4fc869781b8633c9
Nishant Singh <singhnishant414243@gmail.com>
2026-03-20 13:21:03 +0530

    add --local_infile=1

--- scripts/run_dw.bat
+++ scripts/run_dw.bat
@@ -1,14 +1,14 @@
 @echo off
 echo Create and Load Bronze Layer into MySQL...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\bronze.sql
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --local-infile=1 -u root -p1234 bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\bronze.sql
 pause
 
 @echo off
 echo Create and Load Silver Layer into MySQL...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\silver.sql
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --local-infile=1 -u root -p1234 bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\silver.sql
 pause
 
 @echo off
 echo Create and Load Gold Layer into MySQL...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\gold.sql
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --local-infile=1 -u root -p1234 bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\gold.sql
 pause
--- scripts/run_silver.bat
+++ scripts/run_silver.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Create and Load Silver Layer into MySQL...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\silver.sql"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --local-infile=1 -u root -p1234 bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\sql\silver.sql"
 pause
