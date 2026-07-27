a2807a246e39ee794ea0b58f4cd5fba463922ae2
Nishant Singh <singhnishant414243@gmail.com>
2026-07-27 08:03:36 +0530

    Delete scripts/run_test_gold.bat

--- scripts/run_test_gold.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Gold Layer Data Quality Check into MySQL...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --local-infile=1 -u root -p1234 bronze < "C:\Users\TUF\OneDrive\Documents\Code\MY SQL\MySQL-DataWarehouse\test\test_gold.sql"
-pause
